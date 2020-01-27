7e1d47c2cd05d936774bb8ca595f4af480d2ad79
Frederic MAQUET <frederic.maquet@ext.adeo.com>
2020-01-27 22:16:39 +0100

    test(junit) : adding first missing unit tests

---
+++ java/src/test/java/adeo/leroymerlin/cdp/controller/EventControllerTest.java
@@ -0,0 +1,77 @@
+package adeo.leroymerlin.cdp.controller;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import adeo.leroymerlin.cdp.pojo.Event;
+import adeo.leroymerlin.cdp.service.EventService;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class EventControllerTest {
+
+    @Mock
+    private EventService eventService;
+    
+    @InjectMocks
+    private EventController eventController;
+    
+    private MockMvc mockMvc;
+    private ObjectMapper mapper;
+
+	
+    @Before
+    public void init() {
+        mockMvc = MockMvcBuilders.standaloneSetup(eventController).alwaysDo(print()).build();
+        mapper = new ObjectMapper();
+    }
+    
+    @Test
+    public void should_getEvents() throws Exception {
+        
+		Event event1 = new Event();
+		event1.setTitle("GrasPop Metal Meeting");
+		
+		Event event2 = new Event();
+		event2.setTitle("Alcatraz Fest");
+
+		List<Event> eventsFromService =  Arrays.asList(event1,event2);
+    	
+    	// Given the service returns all events
+        when(eventService.getEvents()).thenReturn(eventsFromService);
+
+        // When we call the controller to get all events
+        ResultActions result = mockMvc.perform(get("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON));
+        
+        // Then we check the status 200 and the returned values are the same as the service
+        result.andExpect(status().isOk()).andExpect(content().string(mapper.writeValueAsString(eventsFromService)));      
+        
+		// And the service has been called only once
+        verify(eventService,times(1)).getEvents();
+		verifyNoMoreInteractions(eventService);
+    }
+    
+}
---
+++ java/src/test/java/adeo/leroymerlin/cdp/service/EventServiceTest.java
@@ -0,0 +1,60 @@
+package adeo.leroymerlin.cdp.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import adeo.leroymerlin.cdp.pojo.Event;
+import adeo.leroymerlin.cdp.repository.EventRepository;
+
+@RunWith(SpringRunner.class)
+public class EventServiceTest {
+	
+	@Mock
+	private EventRepository eventRepository;
+	
+	@InjectMocks
+	private EventService eventService;
+	
+	@Before
+	public void init() {
+	}
+	
+	@Test
+	public void should_getEvents(){
+
+		Event event1 = new Event();
+		event1.setTitle("GrasPop Metal Meeting");
+		
+		Event event2 = new Event();
+		event2.setTitle("Alcatraz Fest");
+
+		// Given a list of events on repository
+		List<Event> eventsFromRepository =  Arrays.asList(event1,event2);
+		when(eventRepository.findAllBy()).thenReturn(eventsFromRepository);
+		
+		// When we call the service to get all event
+		List<Event> result = eventService.getEvents();
+		
+		// Then we check the list of events is the same as the repository
+		assertThat(result).isNotNull().isEqualTo(eventsFromRepository);
+		
+		// And the repository has been called only once
+		verify(eventRepository, times(1)).findAllBy();
+		verifyNoMoreInteractions(eventRepository);
+		
+	}
+	
+}
