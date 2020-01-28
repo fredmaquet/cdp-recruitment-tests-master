c1f6cbf06338df1291fc5f0d405006f9c86de3a3
Frederic MAQUET <frederic.maquet@ext.adeo.com>
2020-01-28 08:28:12 +0100

    feat(search) : complete search service

--- java/src/main/java/adeo/leroymerlin/cdp/service/EventService.java
+++ java/src/main/java/adeo/leroymerlin/cdp/service/EventService.java
@@ -3,10 +3,14 @@ package adeo.leroymerlin.cdp.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import adeo.leroymerlin.cdp.pojo.Band;
 import adeo.leroymerlin.cdp.pojo.Event;
+import adeo.leroymerlin.cdp.pojo.Member;
 import adeo.leroymerlin.cdp.repository.EventRepository;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class EventService {
@@ -37,10 +41,33 @@ public class EventService {
 		return eventRepository.save(eventFromRepository);
 	}
 
-    public List<Event> getFilteredEvents(String query) {
-        List<Event> events = eventRepository.findAllBy();
-        // Filter the events list in pure JAVA here
-
-        return events;
-    }
+	public List<Event> getFilteredEvents(String query) {
+		List<Event> events = eventRepository.findAllBy();
+		
+		// Filter the events list in pure JAVA here
+		return events.stream()
+				.map(event -> {
+					List<Band> bandsFiltres = event.getBands().stream()
+							.map(band -> {
+								List<Member> members = band.getMembers().stream()
+										.filter(member -> member.getName().contains(query))
+										.collect(Collectors.toList());
+									band.setMembers(new HashSet<Member>(members));
+									Band newBand = new Band();
+									newBand.setMembers(band.getMembers());
+									newBand.setName(band.getName() + " [" + band.getMembers().size()+"]");
+									return newBand;
+							})
+							.filter(band -> !band.getMembers().isEmpty())
+							.collect(Collectors.toList());
+					event.setBands(new HashSet<Band>(bandsFiltres));
+					
+					long nbChilds = event.getBands().stream()
+					.flatMap(band -> band.getMembers().stream()).count();
+					event.setTitle(event.getTitle() + " [" + nbChilds+"]");
+					return event;
+				})
+				.filter(event -> !event.getBands().isEmpty())
+				.collect(Collectors.toList());
+	}
 }
--- java/src/test/java/adeo/leroymerlin/cdp/service/EventServiceTest.java
+++ java/src/test/java/adeo/leroymerlin/cdp/service/EventServiceTest.java
@@ -6,7 +6,9 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 import org.junit.Before;
@@ -17,7 +19,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import adeo.leroymerlin.cdp.pojo.Band;
 import adeo.leroymerlin.cdp.pojo.Event;
+import adeo.leroymerlin.cdp.pojo.Member;
 import adeo.leroymerlin.cdp.repository.EventRepository;
 
 @RunWith(SpringRunner.class)
@@ -30,9 +34,49 @@ public class EventServiceTest {
 	private EventService eventService;
 	
 	private static final Long EVENT_ID = 1L;
-
+	
+	private Band sum41;
+	private Band pinkFloyd;
+	private Event motocultor;
+	private Event downloadFestival;
+	private Event noBandFestival;
+	
 	@Before
 	public void init() {
+		///// CREATE BAND sum41
+		Member member1Sum41 = new Member();
+		member1Sum41.setName("Queen Charlie Wolf (Chick)");
+		Member member2Sum41 = new Member();
+		member2Sum41.setName("Queen Jamie Petty");
+		Member member3Sum41 = new Member();
+		member3Sum41.setName("Queen Danielle Connor (Dannon)");
+		sum41 = new Band();
+		sum41.setName("Sum41");
+		sum41.setMembers(new HashSet<>(Arrays.asList(member1Sum41, member2Sum41, member3Sum41)));
+
+		////// CREATE BAND pinkFloyd
+		Member member1pinkFloyd = new Member();
+		member1pinkFloyd.setName("Queen Frankie Gross (Fania)");
+		Member member2pinkFloyd = new Member();
+		member2pinkFloyd.setName("Queen Fred Wolf (Chick)");
+		pinkFloyd = new Band();
+		pinkFloyd.setName("Pink flyod");
+		pinkFloyd.setMembers(new HashSet<>(Arrays.asList(member1pinkFloyd, member2pinkFloyd)));
+
+		////// CREATE EVENT Motocultor with band Sum41
+		motocultor = new Event();
+		motocultor.setTitle("Motocultor");
+		motocultor.setBands(new HashSet<>(Arrays.asList(sum41)));
+
+		/////// CREATE EVENT Download Festival with band sum41 and pink floyd
+		downloadFestival = new Event();
+		downloadFestival.setTitle("Download Festival");
+		downloadFestival.setBands(new HashSet<>(Arrays.asList(sum41, pinkFloyd)));
+
+		// CREATE EVENT noBandFestival with no band
+		noBandFestival = new Event();
+		noBandFestival.setTitle("No band festival");
+		noBandFestival.setBands(new HashSet<>());
 	}
 
 	@Test
@@ -108,4 +152,71 @@ public class EventServiceTest {
 		verifyNoMoreInteractions(eventRepository);
 	}
 
+	@Test
+	public void should_filter_event_with_no_result() {
+		// Given the repository returns all events
+		when(eventRepository.findAllBy()).thenReturn(Arrays.asList(motocultor,downloadFestival,noBandFestival));
+
+		// When we filtered all events by unknown member name
+		List<Event> noResult = eventService.getFilteredEvents("NO MATCHING PATTERN");
+		
+		// Then we check the result is empty
+		assertThat(noResult).isEmpty();
+		
+		// And the repository has been called once
+		verify(eventRepository,times(1)).findAllBy();
+		verifyNoMoreInteractions(eventRepository);
+	}
+	
+	@Test
+	public void should_filter_event_with_pinkfloyd_member() {
+		// Given the repository returns all events
+		when(eventRepository.findAllBy()).thenReturn(Arrays.asList(motocultor,downloadFestival,noBandFestival));
+
+		// When we filter events by a known member name (belong to sum41 member)
+		List<Event> eventsFiltered = eventService.getFilteredEvents("Wolf");
+
+		// Then we check the result has two elements(motocultor (sum41) AND downloadfestival (sum41 and pinkfloyd))		
+		assertThat(eventsFiltered).isNotEmpty().hasSize(2);
+		
+		// MOTOCULTOR : checking event
+		Event motocultor_event = eventsFiltered.get(0);
+		assertThat(motocultor_event.getTitle()).isEqualTo("Motocultor [1]");
+		
+		// MOTOCULTOR : checking bands
+		assertThat(motocultor_event.getBands()).isNotEmpty().hasSize(1);
+		List<Band> motocultor_bands = new ArrayList<>(motocultor_event.getBands());
+		assertThat(motocultor_bands.get(0).getName()).isEqualTo("Sum41 [1]");
+		
+		// MOTOCULTOR : Checking member of the band
+		List<Member> sum41_members_motocultor = new ArrayList<>(motocultor_bands.get(0).getMembers());
+		assertThat(sum41_members_motocultor).isNotEmpty().hasSize(1);
+		assertThat(sum41_members_motocultor.get(0).getName()).isEqualTo("Queen Charlie Wolf (Chick)");
+		
+		// DOWNLOAD FESTIVAL : checking event
+		Event downloadFestival_event = eventsFiltered.get(1);
+		assertThat(downloadFestival_event.getTitle()).isEqualTo("Download Festival [2]");
+		
+		// DOWNLOAD FESTIVAL : checking bands
+		assertThat(downloadFestival_event.getBands()).isNotEmpty().hasSize(2);
+		List<Band> downloadFestival_bands = new ArrayList<>(downloadFestival_event.getBands());
+		assertThat(downloadFestival_bands.get(0).getName()).isEqualTo("Sum41 [1]");
+		assertThat(downloadFestival_bands.get(1).getName()).isEqualTo("Pink flyod [1]");
+		
+		// DOWNLOAD FESTIVAL : Checking member of the band (sum41)
+		List<Member> sum41_members_downloadFestival= new ArrayList<>(downloadFestival_bands.get(0).getMembers());
+		assertThat(sum41_members_downloadFestival).isNotEmpty().hasSize(1);
+		assertThat(sum41_members_downloadFestival.get(0).getName()).isEqualTo("Queen Charlie Wolf (Chick)");
+		
+		// DOWNLOAD FESTIVAL : Checking member of the band (pinkFlyod)
+		List<Member> pinkFlyod_members_downloadFestival= new ArrayList<>(downloadFestival_bands.get(1).getMembers());
+		assertThat(pinkFlyod_members_downloadFestival).isNotEmpty().hasSize(1);
+		assertThat(pinkFlyod_members_downloadFestival.get(0).getName()).isEqualTo("Queen Fred Wolf (Chick)");
+		
+		// And the repository has been called once
+		verify(eventRepository,times(1)).findAllBy();
+		verifyNoMoreInteractions(eventRepository);
+		
+	}
+	
 }
